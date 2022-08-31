dd60f9934538bbd0d777efa32f8e0c85504b674d
Jose Miguel Alcaraz <90214208+jose1up@users.noreply.github.com>
2022-08-31 01:24:23 -0300

    fixing css

--- api/src/helpers/product.js
+++ api/src/helpers/product.js
@@ -1,3 +1,4 @@
+import { Op } from "sequelize";
 import { Category } from "../db/models/Category";
 import { OrderDetail } from "../db/models/OrderDetail";
 import { Product } from "../db/models/Product";
@@ -12,10 +13,10 @@ export const createProduct = async (props) => {
 
 export const findProductName = async (props) => {
   try {
+    console.log(props);
     let findProduct = await Product.findAll({
-      include: [Category],
-      include: [OrderDetail_id],
-      where: { name: props },
+      include: [Category, OrderDetail],
+      where: { name: { [Op.iLike]: `%${props}%` } },
     });
     if (findProduct) {
       return findProduct;
@@ -28,8 +29,7 @@ export const findProductName = async (props) => {
 export const allProducts = async () => {
   try {
     const allProduct = await Product.findAll({
-      include: [Category],
-      includes: [OrderDetail],
+      include: [Category, OrderDetail],
     });
     return allProduct;
   } catch (error) {
