66e52ece854b0863cfe098d6bc27ff586e15664f
turrisi <dip.aturrisi@gmail.com>
2022-08-31 17:38:24 -0300

    Form first styles

--- client/src/components/counter.jsx
+++ client/src/components/counter.jsx
@@ -2,6 +2,19 @@ import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllCategory } from "../redux/actions/category";
 import { createProduct, getAllProduct } from "../redux/actions/products";
+import {
+    FormControl,
+    FormLabel,
+    FormErrorMessage,
+    FormHelperText,
+    Button,
+    ButtonGroup,
+    Tabs,
+    TabList,
+    TabPanels,
+    Tab,
+    TabPanel
+} from '@chakra-ui/react'
 
 export const NewProduct = () => {
     const dispatch = useDispatch();
@@ -46,30 +59,41 @@ export const NewProduct = () => {
     }
 
     return (
-        <div>
-            <form onSubmit={(e) => handleSubmit(e)}>
-                <label >Product</label>
-                <input placeholder="product" type={"Text"} name="name" onChange={(e) => handleInputChange(e)} />
-                <label >Category</label>
-                <select name="Category_id" onChange={(e) => handleSelectChange(e)}>
-                    <option key={0} value="none"></option>
-                    {categorys && categorys.map((cat) => {
-                        return (
-                            <option
-                                key={cat.id}
-                                value={cat.name}>{cat.name}
-                            </option>
-                        )
-                    })}
-                </select>
-                <label >Description</label>
-                <input placeholder="Description" type={"text"} name="description" onChange={(e) => handleInputChange(e)} />
-                <label >Price</label>
-                <input placeholder="Price" type={"number"} name="price" onChange={(e) => handleInputChange(e)} />
-                <label >Image</label>
-                <input placeholder="Image" type={"url"} name="img" onChange={(e) => handleInputChange(e)} />
-                <button type="submit">Submit</button>
-            </form>
-        </div>
+        <Tabs isFitted variant='enclosed'>
+            <TabList mb='1em'>
+                <Tab>New Product</Tab>
+                <Tab>New Category</Tab>
+            </TabList>
+            <TabPanels>
+                <TabPanel>
+                    <FormControl onSubmit={(e) => handleSubmit(e)}>
+                        <FormLabel >Product</FormLabel>
+                        <input placeholder="product" type={"Text"} name="name" onChange={(e) => handleInputChange(e)} />
+                        <FormLabel >Category</FormLabel>
+                        <select placeholder="Select one" name="Category_id" onChange={(e) => handleSelectChange(e)}>
+                            <option key={0} value="none"></option>
+                            {categorys && categorys.map((cat) => {
+                                return (
+                                    <option
+                                        key={cat.id}
+                                        value={cat.name}>{cat.name}
+                                    </option>
+                                )
+                            })}
+                        </select>
+                        <FormLabel >Description</FormLabel>
+                        <input placeholder="Description" type={"text"} name="description" onChange={(e) => handleInputChange(e)} />
+                        <FormLabel >Price</FormLabel>
+                        <input placeholder="Price" type={"number"} name="price" onChange={(e) => handleInputChange(e)} />
+                        <FormLabel >Image</FormLabel>
+                        <input placeholder="Image" type={"url"} name="img" onChange={(e) => handleInputChange(e)} />
+                        <Button type="submit">Submit</Button>
+                    </FormControl>
+                </TabPanel>
+                <TabPanel>
+                    <h2>Not available yet</h2>
+                </TabPanel>
+            </TabPanels>
+        </Tabs>
     )
 }
